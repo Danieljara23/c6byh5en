198854914fd311ff3f7ac27be58f6f08a60534c5
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-03-14 11:32:25 -0500

    Fix issue for Make It's evaluator

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
   namespace :api do
     namespace :v1 do
-      resources :products, only: [:index]
+      resources :products, format: 'json'
     end
   end
 end
