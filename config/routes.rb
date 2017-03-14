1675d07b5c980a275dc0cee0139e2beb14529d00
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-03-14 11:26:30 -0500

    Fix json order for evaluator on Make it Website

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
   namespace :api do
     namespace :v1 do
-      resources :products, defaults: {format: 'json'}, only: [:index]
+      resources :products, only: [:index]
     end
   end
 end
